080057c97055ab3f310c612e190816bdc536baee
devmarlosgomes <corretormarlosgomes@gmail.com>
2024-12-04 21:42:10 -0300

    relampago

---
+++ while/while.js
@@ -0,0 +1,35 @@
+// let contador = 1;       // verifica a condicao antes da primeira execucao
+// while (contador <= 1000000) {
+//   console.log("Contagem:", contador);
+//   contador++;
+// }
+
+// let numero = 1;
+// do {             // primeiro executa, só depois faz a primeira verificação
+//   console.log("Número:", numero);
+//   numero++;
+//   if(numero == 10){break}
+// } while (numero <= 15);
+
+// usando do...while pra pedir o numero enquanto nao for valido
+// const prompt = require('prompt-sync')();
+// let entrada;
+// do {
+//   entrada = Number(prompt("Digite um número entre 1 e 10: "));
+// } while (entrada < 1 || entrada > 10);
+
+// console.log("Número válido:", entrada);
+
+
+// usando while pra pedir o numero enquanto nao for valido
+// const prompt = require('prompt-sync')();
+// primeira = true
+// while(entrada < 0 || entrada > 10){
+//     if(primeira){
+//         entrada = Number(prompt("Digite um número entre 1 e 10: "))
+//     }
+//     else{
+//         entrada = Number(prompt("Numero invalido!! Digite um número entre 1 e 10: "))}
+//         primeira = false
+// }
+// console.log("Número válido:", entrada)
